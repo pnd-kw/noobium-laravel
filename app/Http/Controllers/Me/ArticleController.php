06fb316c76bedac51ecb18ed09689fabfbfe94ff
Pandu KW <pandu.usahaku@gmail.com>
2023-12-01 21:13:40 +0700

    show fetch article by user

--- app/Http/Controllers/Me/ArticleController.php
+++ app/Http/Controllers/Me/ArticleController.php
@@ -8,9 +8,40 @@ use App\Http\Requests\Me\Article\StoreRequest;
 use Str;
 use ImageKit\ImageKit;
 use App\Models\User;
+use App\Models\Article;
 
 class ArticleController extends Controller
 {
+    public function index()
+    {
+        // get user id yg saat ini sedang login
+        // get article dimana user id nya yg saat ini sedang login
+        // get juga category dan usernya siapa
+        // buat paginationnya
+
+        $userId = auth()->id();
+
+        $articles = Article::with(['category', 'user:id,name,email,picture'])->select([
+            'id', 
+            'user_id', 
+            'category_id', 
+            'title', 
+            'slug', 
+            'content_preview', 
+            'featured_image', 
+            'created_at', 
+            'updated_at',
+        ])->where('user_id', $userId)->paginate();
+
+        return response()->json([
+            'meta' => [
+                'code' => 200,
+                'status' => 'success',
+                'message' => 'Articles fetched successfully',
+            ],
+            'data' => $articles,
+        ]);
+    }
     public function store(StoreRequest $request)
     {
         // category_id
--- app/Models/Article.php
+++ app/Models/Article.php
@@ -18,4 +18,14 @@ class Article extends Model
         'content',
         'featured_image',
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
